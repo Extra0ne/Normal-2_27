1ee4f2268d8ec46a65480ef46ffbbad94e26fd78
Vasya Bogatchuk <v380962437840@gmail.com>
2020-08-12 19:45:40 +0300

    no message

---
+++ Normal-2_27/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normal_2_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Если вы  хотите завершить программу, нажимите |0|");
+                Console.WriteLine("Введите число : ");
+                Console.Write("-> ");
+
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num % 2 == 0 || num % 3 == 0 && num != 2 && num != 3 && num !=0)
+                {
+                    Console.WriteLine("||Это составное число||");
+                }
+                else
+                {
+                    Console.WriteLine("||Это простое число||");
+                }
+            } while (num !=0);
+                Console.ReadKey();
+        }
+    }
+}
